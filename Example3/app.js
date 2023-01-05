c213c088695a14109d0e444ab83cf7ceccaa6153
Monika Szucs <113265275+mszucseducation@users.noreply.github.com>
2023-01-04 20:42:12 -0800

    adding typescript examples

--- Example3/app.js
+++
@@ -1,8 +0,0 @@
-// you shouldn't do: const person: object
-// TS notation for object represented at {}
-var person = {
-    name: "John",
-    age: 30
-};
-console.log(person);
-console.log(person.name);
